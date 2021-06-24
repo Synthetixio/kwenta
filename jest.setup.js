4328373891a68f92e4fa2820c842eec8cb1b71d0
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-06-24 10:05:19 -0400

    feat: added jsdom support for crypto

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,17 @@
 import '@testing-library/jest-dom';
+import crypto from 'crypto';
 
 window.matchMedia = (media) => ({
 	addListener: () => {},
 	removeListener: () => {},
 	matches: true,
 });
+
+Object.defineProperty(global.self, 'crypto', {
+    value: {
+      getRandomValues: arr => crypto.randomBytes(arr.length)
+    }
+  });
 
 jest.mock('react-optimized-image', () => ({
 	__esModule: true,
