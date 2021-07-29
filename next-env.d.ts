01e74b4bdf36d5fe4cd1d816b303446cadfdc912
Kevin Meraz <kevinemeraz@gmail.com>
2021-07-28 23:51:03 -0500

    upgrade react, react-dom, nextjs and update some image optimization techniques to use nextjs' own next/image

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/// <reference types="optimized-images-loader" />
-
-export {};
+/// <reference types="next/image-types/global" />
--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
 // next.config.js
 
 module.exports = {
-	webpack: (config, options) => {
-
+	webpack: (config) => {
 		config.resolve.mainFields = ['module', 'browser', 'main'];
-
-		config.module.rules.push({
-			test: /\.(png|jp(e*)g|svg|gif|webp)$/,
-			use: [
-				{
-					loader: 'optimized-images-loader',
-					options: {
-						includeStrategy: 'react',
-						publicPath: `/_next/static/images/`,
-						outputPath: 'static/images',
-					},
-				},
-			],
-		});
 		return config;
 	},
 	trailingSlash: !!process.env.NEXT_PUBLIC_DISABLE_PRETTY_URLS,
