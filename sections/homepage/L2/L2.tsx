1cac81f0cf1ec0d44453d50fc2b42560e2a36820
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-08-18 17:40:35 -0500

    feat: L2 section on landing page

--- sections/homepage/L2/L2.tsx
+++ sections/homepage/L2/L2.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import Img, { Svg } from 'react-optimized-image';
-
-import SlippageIcon from 'assets/svg/marketing/icon-slippage.svg';
-import InfiniteLiquidityIcon from 'assets/svg/marketing/icon-infinite-liquidity.svg';
-import FuturesIcon from 'assets/svg/marketing/icon-futures.svg';
+import Img from 'react-optimized-image';
 
 import {
 	FlexDiv,
@@ -16,108 +12,121 @@ import {
 } from 'styles/common';
 
 import { Copy, FlexSection, GridContainer, LeftSubHeader, Title } from '../common';
-import media from 'styles/media';
+import media, { Media } from 'styles/media';
 import Button from 'components/Button';
 
-import Optimism from 'assets/svg/marketing/OPTIMISM-R.svg';
-import LINKIcon from 'assets/svg/currencies/crypto/LINK.svg';
-import ETHIcon from 'assets/svg/currencies/crypto/ETH.svg';
-import USDIcon from 'assets/svg/currencies/fiat/USD.svg';
-import BTCIcon from 'assets/svg/currencies/crypto/BTC.svg';
-
-const FEATURES = [
-	{
-		id: 'infinite-liquidity',
-		title: 'homepage.features.infinite-liquidity.title',
-		copy: 'homepage.features.infinite-liquidity.copy',
-		image: <Img src={InfiniteLiquidityIcon} alt="" />,
-	},
-	{
-		id: 'zero-slippage',
-		title: 'homepage.features.zero-slippage.title',
-		copy: 'homepage.features.zero-slippage.copy',
-		image: <Img src={SlippageIcon} alt="" />,
-	},
-	{
-		id: 'synthetic-futures',
-		title: 'homepage.features.synthetic-futures.title',
-		copy: 'homepage.features.synthetic-futures.copy',
-		image: <Img src={FuturesIcon} alt="" />,
-		comingSoon: true,
-	},
-];
+import Optimism from 'assets/svg/marketing/OPTIMISM_alpha.svg';
+import TransactionSpeedNumber from 'assets/svg/marketing/1.svg';
+import TransactionCostNumber from 'assets/svg/marketing/50x.svg';
+import Currency from 'components/Currency';
 
 const L2 = () => {
 	const { t } = useTranslation();
 
+	const OptimismStats = (
+		<div>
+			<FlexDivCol>
+				<OptimismTag>Powered by</OptimismTag>
+				<Img src={Optimism} width={184} />
+			</FlexDivCol>
+			<StyledGridContainer>
+				<FeatureCard>
+					<Golden>
+						<Img src={TransactionSpeedNumber} height={48.5} />
+					</Golden>
+					<FeatureContentTitle>
+						<Title>{t('homepage.l2.transaction-speed')}</Title>
+					</FeatureContentTitle>
+				</FeatureCard>
+				<FeatureCard>
+					<Golden>
+						<Img src={TransactionCostNumber} height={48.5} />
+					</Golden>
+					<FeatureContentTitle>
+						<Title>{t('homepage.l2.transaction-cost')}</Title>
+					</FeatureContentTitle>
+				</FeatureCard>
+			</StyledGridContainer>
+			<Disclaimer>
+				<Copy>{t('homepage.l2.disclaimer')}</Copy>
+			</Disclaimer>
+		</div>
+	);
+
 	return (
 		<Container>
-			<FlexSection>
-				<div>
-				<FlexDivCol>
-						<p>Powered by</p>
-						<Img src={Optimism} width={138}/>
-						</FlexDivCol>
-				<StyledGridContainer>
-						
-						<FeatureCard>
-							<Golden>10</Golden>
-							<FeatureContentTitle>
-								<Title>Transactions per second</Title>
-							</FeatureContentTitle>
-						</FeatureCard>
-				</StyledGridContainer>
-				</div>
-				<FlexDivCol>
-					<StyledLeftSubHeader>{t('homepage.l2.title')}</StyledLeftSubHeader>
-					<Copy>
-						<Trans i18nKey={'homepage.l2.copy'} components={[<Emphasis />]} />
-					</Copy>
-					<FlexDiv>
-						<Button variant={'outline'}>{t('homepage.l2.cta-buttons.learn-more')}</Button>
-						<Button variant={'primary'}>{t('homepage.l2.cta-buttons.switch-l2')}</Button>
-					</FlexDiv>
-				</FlexDivCol>
-			</FlexSection>
-			<FlexSection>
-				<FlexDivCol>
-					<StyledLeftSubHeader>{t('homepage.l2.markets.title')}</StyledLeftSubHeader>
-					<Copy>{t('homepage.l2.markets.copy')}</Copy>
-				</FlexDivCol>
-				<StyledGridContainer>
-					<FlexDivColCentered>
-						<Img src={LINKIcon} height={50} />
-						<Copy>sLINK</Copy>
-					</FlexDivColCentered>
-					<FlexDivColCentered>
-						<Img src={ETHIcon} height={50} />
-						<Copy>sETH</Copy>
-					</FlexDivColCentered>
-					<FlexDivColCentered>
-						<Img src={USDIcon} height={50} />
-						<Copy>sUSD</Copy>
-					</FlexDivColCentered>
-					<FlexDivColCentered>
-						<Img src={BTCIcon} height={50} />
-						<Copy>sBTC</Copy>
-					</FlexDivColCentered>
-				</StyledGridContainer>
-			</FlexSection>
+			<MiniContainer>
+				<FlexSection>
+					<Media greaterThanOrEqual="lg">{OptimismStats}</Media>
+					<FlexDivCol>
+						<StyledLeftSubHeader>{t('homepage.l2.title')}</StyledLeftSubHeader>
+						<L2Copy>
+							<Trans i18nKey={'homepage.l2.copy'} components={[<Emphasis />]} />
+						</L2Copy>
+						<Media lessThan="lg">{OptimismStats}</Media>
+						<CTARow>
+							<Button variant={'outline'} size={'lg'}>
+								{t('homepage.l2.cta-buttons.learn-more')}
+							</Button>
+							<Button variant={'primary'} size={'lg'}>
+								{t('homepage.l2.cta-buttons.switch-l2')}
+							</Button>
+						</CTARow>
+					</FlexDivCol>
+				</FlexSection>
+			</MiniContainer>
+			<MiniContainer>
+				<LiveMarkets>
+					<FlexGrowCol>
+						<LeftSubHeader>{t('homepage.l2.markets.title')}</LeftSubHeader>
+						<Copy>{t('homepage.l2.markets.copy')}</Copy>
+					</FlexGrowCol>
+					<FlexGrowCol>
+						<FlexDivRow>
+							<FlexDivColCentered>
+								<Currency.Icon currencyKey={'sLINK'} height={'50px'} width={'50px'} />
+								<SynthCaption>sLINK</SynthCaption>
+							</FlexDivColCentered>
+							<FlexDivColCentered>
+								<Currency.Icon currencyKey={'sETH'} height={'50px'} width={'50px'} />
+								<SynthCaption>sETH</SynthCaption>
+							</FlexDivColCentered>
+							<FlexDivColCentered>
+								<Currency.Icon currencyKey={'sUSD'} height={'50px'} width={'50px'} />
+								<SynthCaption>sUSD</SynthCaption>
+							</FlexDivColCentered>
+							<FlexDivColCentered>
+								<Currency.Icon currencyKey={'sBTC'} height={'50px'} width={'50px'} />
+								<SynthCaption>sBTC</SynthCaption>
+							</FlexDivColCentered>
+						</FlexDivRow>
+					</FlexGrowCol>
+				</LiveMarkets>
+			</MiniContainer>
 		</Container>
 	);
 };
 
-const Golden = styled.p`
-	font-size: 55px;
-	font-weight: 700;
-	background: ${props => props.theme.colors.gold};
-	background-clip: text;
-	text-fill-color: transparent;
-	background-size: 100%;
-    -webkit-background-clip: text;
-    -moz-background-clip: text;
-    -webkit-text-fill-color: transparent; 
-    -moz-text-fill-color: transparent;
+const LiveMarkets = styled(FlexSection)``;
+
+const L2Copy = styled(Copy)`
+	margin-bottom: 48px;
+	${media.lessThan('lg')`
+		margin-bottom: 24px;
+	`}
+`;
+
+const OptimismTag = styled(Copy)`
+	margin-bottom: 6px;
+`;
+
+const Disclaimer = styled.div`
+	width: 80%;
+	margin: 48px 0;
+`;
+
+const Golden = styled.div`
+	margin-bottom: 10px;
 `;
 
 const Emphasis = styled.b`
@@ -128,21 +137,32 @@ const StyledGridContainer = styled(GridContainer)`
 	${media.lessThan('lg')`
 		grid-template-columns: repeat(4, auto);
 	`}
-	${media.lessThan('md')`
-		grid-template-columns: auto;
-	`}
+`;
+
+const FlexGrowCol = styled(FlexDivCol)`
+	flex: 1;
+	width: 100%;
+	margin: 24px 0;
 `;
 
 const Container = styled.div`
-	padding-bottom: 240px;
+	margin-bottom: 150px;
 	${media.lessThan('lg')`
-		padding-bottom: 140px;
+		margin-bottom: 75px;
+	`}
+`;
+
+const MiniContainer = styled.div`
+	padding-bottom: 75px;
+	${media.lessThan('lg')`
+		padding-bottom: 37.5px;
 	`}
 `;
 
 const StyledLeftSubHeader = styled(LeftSubHeader)`
 	max-width: 500px;
-	padding-top: 80px;
+	padding-top: 10px;
+	padding-bottom: 10px;
 	${media.lessThan('lg')`
 		padding-top: 0;
 		padding-bottom: 56px;
@@ -150,14 +170,13 @@ const StyledLeftSubHeader = styled(LeftSubHeader)`
 `;
 
 const FeatureCard = styled(FlexDivCol)`
+	margin-top: 64px;
 	margin-bottom: 16px;
 `;
 
-const FeatureIconContainer = styled.div`
-	padding-bottom: 40px;
-	img {
-		width: 64px;
-		height: 64px;
+const CTARow = styled(FlexDiv)`
+	> * {
+		margin-right: 16px;
 	}
 `;
 
@@ -165,15 +184,8 @@ const FeatureContentTitle = styled(FlexDivCentered)`
 	padding-bottom: 14px;
 `;
 
-const ComingSoonTag = styled(FlexDivCentered)`
-	width: 104px;
-	height: 24px;
-	background: ${(props) => props.theme.colors.gold};
-	border-radius: 50px;
-	color: ${(props) => props.theme.colors.white};
-	font-family: ${(props) => props.theme.fonts.bold};
-	justify-content: center;
-	margin-left: 16px;
+const SynthCaption = styled(Copy)`
+	margin-top: 12px;
 `;
 
 export default L2;
